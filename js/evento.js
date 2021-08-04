933f9e174d37d9b405e1833a1caa2c265012aba3
Osmar De Leon <osmar.oficial.619@gmail.com>
2021-08-03 21:48:32 -0600

    paginacion completa

--- js/evento.js
+++ js/evento.js
@@ -1,9 +1,11 @@
-import{remover,preUrl} from './funciones.js';
+import{remover,preUrl,conexion} from './funciones.js';
 
 
 let d = document, $nombre = d.getElementById("bPersonaje"),$formulario = d.getElementById("formulario"),
-    $gender = document.getElementById("gender"),$status = document.getElementById("status");
-$formulario.addEventListener("submit",(e) => {
+    $gender = document.getElementById("gender"),$status = document.getElementById("status"),
+    $next = d.getElementById("next"),$prev = d.getElementById("prev"),contador = 1;
+
+    $formulario.addEventListener("submit",(e) => {
     e.preventDefault();
     remover($nombre.value);
 });
@@ -15,4 +17,16 @@ $gender.addEventListener("change",() => {
 
 $status.addEventListener("change",() => {
     preUrl($nombre.value);
+});
+
+$next.addEventListener("click",(e)=> {
+    e.preventDefault();
+    contador++;
+    conexion($next.href,contador);
+});
+
+$prev.addEventListener("click",(e)=>{
+    e.preventDefault();
+    contador--;
+    conexion($prev.href,contador);
 });
--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export const remover = (texto) =>{
-    let newText = texto.trim(),respuesta;
+    let newText = texto.trim();
     if(newText === "" && $gender.value === 'gender' && $status.value === 'status'){
         swal("Ocurrío un Error", "Por favor Ingresar un nombre", "error");
     }
@@ -11,18 +11,22 @@ export const remover = (texto) =>{
 }
 
 let $gender = document.getElementById("gender"),$status = document.getElementById("status"),
-    $total = document.getElementById('totalResul'),$contResul = document.getElementById('resul');
+    $total = document.getElementById('totalResul'),$contResul = document.getElementById('resul'),
+    $nextt = document.getElementById("next"),$prevv = document.getElementById("prev"),
+    $pag = document.getElementById('pag'),$paginacion = document.getElementById("paginacion");
 
-const conexion = async (url) => {
+export const conexion = async (url,contador = 1) => {
     let $ultimo = $contResul.lastElementChild;
-    const con = await fetch(`https://rickandmortyapi.com/api/character/${url}`);
+    const con = await fetch(url);
     if(con.status !== 200)
     {
         swal("Sin Resultados :(", "", "error");
         $contResul.removeChild($ultimo);
         $total.textContent = "";
+        $paginacion.classList.add("disabled");
         return;
     }
+    
     if($ultimo.className === 'resultados') $contResul.removeChild($ultimo);
     const res = await con.json();
     $total.textContent = `Resultados: ${res.info.count}`;
@@ -37,21 +41,25 @@ const conexion = async (url) => {
         $img.setAttribute("src",valor.image);
         $img.setAttribute("alt",`Imagen de ${valor.name}`);
         const $gen = document.createElement("div");
-        if(valor.gender === 'Female')
+        switch(valor.gender)
         {
-            $gen.setAttribute('class','gen female')
-            $gen.innerHTML = `<i class="fas fa-female"></i>`;
-        }else if(valor.gender === 'Male'){
-            $gen.setAttribute('class','gen male')
-            $gen.innerHTML = `<i class="fas fa-male"></i>`;
-        }else{
-            $gen.setAttribute('class','gen unknow')
-            $gen.innerHTML = `<i class="fas fa-question"></i>`;
+            case 'Female':
+                $gen.setAttribute('class','gen female')
+                $gen.innerHTML = `<i class="fas fa-female"></i>`;
+                break;
+            case 'Male':
+                $gen.setAttribute('class','gen male')
+                $gen.innerHTML = `<i class="fas fa-male"></i>`;
+                break;
+            default:
+                $gen.setAttribute('class','gen unknow')
+                $gen.innerHTML = `<i class="fas fa-question"></i>`;
+                break;
         }
         const $div = document.createElement("div");
         $div.innerHTML = `
             <p>Nombre: <span class="datos">${valor.name}</span></p>
-            <p>Estado: <span class="datos">${valor.status}</span></p>
+            <p>Estado: ${estado(valor.status)}</p>
             <p>G&eacute;nero: <span class="datos">${valor.gender}</span></p>
             <p>Especie: <span class="datos">${valor.species}</span></p>
             <p>Origen: <span class="datos">${valor.location.name}</span></p>
@@ -63,50 +71,87 @@ const conexion = async (url) => {
     });
     $divR.appendChild($fragmento)
     $contResul.appendChild($divR);
+    $paginacion.classList.remove("dissabled");
+    $pag.textContent = `${contador}/${res.info.pages}`;
 
+    switch(res.info.next){
+        case null:
+            $nextt.classList.add('disabled');
+            break;
+        default:
+            $nextt.href=res.info.next;
+            $nextt.classList.remove('disabled');
+            break;
+    }
+    switch(res.info.prev){
+        case null:
+            $prevv.classList.add("disabled");
+            break
+        default:
+            $prevv.href = res.info.prev;
+            $prevv.classList.remove("disabled");
+            break;
+    }
+}
+
+const estado = (es) => {
+    let respuesta;
+    switch(es){
+        case 'Alive':
+            respuesta = `<span class="a alive">${es}</span>`;
+            break;
+        case 'Dead':
+            respuesta = `<span class="a dead">${es}</span>`;
+            break;
+        default:
+            respuesta = `<span class="a des">${es}</span>`;
+            break;
+    }
+    return respuesta;
 }
 
 export const preUrl = (personaje = "") => {
-    let url;
+    let url = `https://rickandmortyapi.com/api/character/`;
     if(personaje !== "" && $gender.value === 'gender' && $status.value === 'status')
     {
-        url = `?name=${personaje}`;
+        url += `?name=${personaje}`;
     }
     else if(personaje !== '' && $gender.value !== 'gender' && $status.value === 'status'){
-        url = `?name=${personaje}&gender=${$gender.value}`;
+        url += `?name=${personaje}&gender=${$gender.value}`;
     }
     else if(personaje !== '' && $status.value !== 'status' && $gender.value === 'gender'){
-        url = `?name=${personaje}&status=${$status.value}`;
+        url += `?name=${personaje}&status=${$status.value}`;
     }
     else if($gender.value !== 'gender' && personaje === "" && $status.value === 'status')
     {
-        url = `?gender=${$gender.value}`;
+        url += `?gender=${$gender.value}`;
     }
     else if($gender.value !== 'gender' && personaje !== '' && $status.value === 'status')
     {
-        url = `?name=${personaje}&gender=${$gender.value}`
+        url += `?name=${personaje}&gender=${$gender.value}`
     }
     else if($gender.value !== 'gender' && $status.value !== 'status' && personaje === '')
     {
-        url = `?gender=${$gender.value}&status=${$status.value}`;
+        url += `?gender=${$gender.value}&status=${$status.value}`;
     }
     else if($status.value !== 'status' && personaje === "" && $gender.value === 'gender')
     {
-        url = `?status=${$status.value}`;
+        url += `?status=${$status.value}`;
     }
     else if($status.value !== 'status' && personaje !== '' && $gender.value === 'gender')
     {
-        url = `?name=${personaje}&status=${$status.value}`;
+        url += `?name=${personaje}&status=${$status.value}`;
     }
     else if(personaje !== '' && $status.value !== 'status' && $gender.value !== 'gender')
     {
-        url = `?name=${personaje}&status=${$status.value}&gender=${$gender.value}`;
+        url += `?name=${personaje}&status=${$status.value}&gender=${$gender.value}`;
     }
     else{
         let $ultimo = $contResul.lastElementChild;
         $contResul.removeChild($ultimo);
         $total.textContent = "";
-        swal("Ocurrío un Error", "Por favor Ingresar un nombre o seleccione una opción", "error");
+        $paginacion.classList.add("disabled");
+        swal("Ocurrío un Error", "Por favor Ingrese un nombre o seleccione una opción", "error");
         return;
     }
     conexion(url);
